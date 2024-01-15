15e099746f44b5b733da2862aeffb6d9d5a44f92
a-shirshov <artyomsh01@yandex.ru>
2024-01-16 00:11:00 +0300

    next-js started

---
+++ components/Header.jsx
@@ -0,0 +1,10 @@
+import { ConnectButton } from "web3uikit";
+
+export default function Header() {
+    return (
+        <div>
+            Decentralised Lottery
+            <ConnectButton moralisAuth={false}></ConnectButton>
+        </div>
+    )
+}
